8d9bc37c46a0b7a65a63ed7905e1122670fafa3b
Rainier King <rainier.king@odevsolutions.com>
2020-03-23 11:17:34 +0000

    [ADD] ausphin_special

---
+++ ausphin_special/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Ausphin Special',
+    'version': '0.1',
+    'depends': [
+        'crm',
+    ],
+    'external_dependencies': {},
+    'author': 'Ausphin',
+    'website': '',
+    'summary': """Special Modifications for Ausphin""",
+    'description': """
+        Special Modifications for Ausphin
+    """,
+    'category': 'Special',
+    'data': [
+        'views/crm_lead_views.xml',
+    ],
+    'qweb': [],
+    'css': [],
+    'images': [],
+    'demo': [],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}

451951eda7780d9f47ad160b9f4deff4ad8c38cc
rainierking <rainier.king@odevsolutions.com>
2020-03-27 03:11:37 +0000

    [IMP] lead_stage_duration - add color for duration in kanban to easily identify if within or beyond - remove target_duration in stage log since it is visible in stage

--- lead_stage_duration/models/crm_lead_stage_log.py
+++ lead_stage_duration/models/crm_lead_stage_log.py
@@ -25,8 +25,6 @@ class CrmLeadStageLog(models.Model):
     end_date = fields.Datetime(string="End Date")
     actual_duration = fields.Float(string="Actual Duration",
         compute="_compute_actual_duration")
-    target_duration = fields.Float(string="Target Duration",
-        related="stage_id.target_duration")
     user_id = fields.Many2one(string="Assignee",
         comodel_name="res.users")
     stage_id = fields.Many2one(string="Stage",

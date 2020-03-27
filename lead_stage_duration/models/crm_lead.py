a4a3214851ef41fafc785fde5814899f39328a11
rainierking <rainier.king@odevsolutions.com>
2020-03-27 01:44:14 +0000

    [IMP] lead_stage_duration - auto create log entry when changing stage or user - compute total duration for current stage

--- lead_stage_duration/models/crm_lead.py
+++ lead_stage_duration/models/crm_lead.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+from datetime import datetime
+
 from odoo import models, fields, api
 
 class CrmLead(models.Model):
@@ -18,14 +20,39 @@ class CrmLead(models.Model):
     stage_log_ids = fields.One2many(string="Stage Logs",
         comodel_name="crm.lead.stage.log",
         inverse_name="lead_id")
+    duration_in_stage = fields.Float(string="Duration in Stage",
+        compute="_compute_duration_in_stage")
     
     ##############################
     # Compute and search methods #
     ##############################
+    @api.depends("stage_log_ids")
+    def _compute_duration_in_stage(self):
+        for lead in self:
+            lead.duration_in_stage = \
+                sum(log.actual_duration for log in \
+                    lead.stage_log_ids.filtered(lambda l: l.stage_id == lead.stage_id))
     
     ############################
     # Constrains and onchanges #
     ############################
+    @api.constrains("stage_id", "user_id")
+    def _record_stage_log(self):
+        current_time = datetime.now()
+        for lead in self:
+            # end last log
+            if lead.stage_log_ids:
+                last_log = lead.stage_log_ids[-1]
+                last_log.end_date = current_time
+
+            # create new log
+            log_obj = self.env["crm.lead.stage.log"]
+            log_obj.create({
+                "lead_id": lead.id,
+                "stage_id": lead.stage_id.id,
+                "user_id": lead.user_id and lead.user_id.id,
+                "start_date": current_time,
+            })
 
     #########################
     # CRUD method overrides #

dc1f5e8542268fa5168de3f3caca7b9055978ddf
rainierking <rainier.king@odevsolutions.com>
2020-03-29 02:44:42 +0000

    [ADD] crm_training: initial commit

---
+++ crm_training/models/crm_training_venue.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+class CrmTrainingVenue(models.Model):
+    ######################
+    # Private attributes #
+    ######################
+    _name = "crm.training.venue"
+    _description = "Training Venue"
+    _order = "name"
+    
+    ###################
+    # Default methods #
+    ###################
+
+    ######################
+    # Fields declaration #
+    ######################
+    name = fields.Char(string="Name",
+        required=True)
+    trainee_ids = fields.One2many(string="Trainees",
+        comodel_name="crm.training",
+        inverse_name="venue_id")
+    
+    ##############################
+    # Compute and search methods #
+    ##############################
+    
+    ############################
+    # Constrains and onchanges #
+    ############################
+
+    #########################
+    # CRUD method overrides #
+    #########################
+
+    ##################
+    # Action methods #
+    ##################
+
+    ####################
+    # Business methods #
+    ####################

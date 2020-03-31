b237198b305f36d0053b56a2df49f5554835daf0
rainierking <rainier.king@odevsolutions.com>
2020-03-31 03:43:44 +0000

    [IMP] ausphin_special: hide opportunities for other journeys for sales all documents access level

--- ausphin_special/__manifest__.py
+++ ausphin_special/__manifest__.py
@@ -21,6 +21,7 @@
     'category': 'Special',
     'data': [
         'security/crm_lead_security.sql',
+        'security/crm_lead_security.xml',
         'security/crm_team_security.xml',
         'views/crm_lead_views.xml',
         'views/crm_team_views.xml',

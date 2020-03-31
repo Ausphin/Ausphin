f5d2004a000ba34748ee0f3c084419901b2c70a5
rainierking <rainier.king@odevsolutions.com>
2020-03-31 02:26:18 +0000

    [IMP] ausphin_special: hide move buttons if not assigned or a manager, random assign for first stages, redirect to list after conversion

--- ausphin_special/wizards/crm_lead2opportunity_partner.py
+++ ausphin_special/wizards/crm_lead2opportunity_partner.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-import random
+
 from odoo import fields, models, api
 
 class Lead2OpportunityPartner(models.TransientModel):
@@ -22,48 +22,21 @@ class Lead2OpportunityPartner(models.TransientModel):
     
     ##############################
     # Compute and search methods #
-    ##############################        
-        
+    ##############################
 
     ############################
     # Constrains and onchanges #
     ############################
-
     @api.onchange("team_id")
     def _onchange_sales_team(self):
-        self.user_id = False
-    
-        min = 999999999
-        results = []
-        
-        stage = self.env["crm.stage"].sudo().search([("team_id","=",self.team_id.id)], limit=1).id
-        stage_id = self.env["crm.stage"].browse(stage)
-        self.stage_id = stage_id.id
-        
-                        
-        for assignable_id in stage_id.assignable_ids:
-            opportunity_count = self.env["crm.lead"].sudo().search_count([("stage_id","=",stage_id.id),("user_id","=",assignable_id.id)])
-            if not opportunity_count:
-                opportunity_count = 0
-            
-            if opportunity_count < min:
-                min = opportunity_count
-                results = [assignable_id.id]
-            elif opportunity_count == min:
-                results.append(assignable_id.id)
-        
-        if len(results) > 1:
-            self.user_id = random.choice(results)
-        
-        elif len(results) == 1:
-            self.user_id = results[0]
-            
-        
-        
+        stage_id = self.env["crm.stage"].sudo().search([("team_id","=",self.team_id.id)], limit=1).id
+        stage = self.env["crm.stage"].browse(stage_id)
+        self.stage_id = stage_id
+        self.user_id = stage.get_assignee()
+
     #########################
     # CRUD method overrides #
     #########################
-    
     
     ##################
     # Action methods #
@@ -71,7 +44,8 @@ class Lead2OpportunityPartner(models.TransientModel):
     @api.multi
     def action_apply(self):
         self.ensure_one()
-        return super(Lead2OpportunityPartner, self.with_context(stage_id=self.stage_id.id)).action_apply()
+        super(Lead2OpportunityPartner, self.with_context(stage_id=self.stage_id.id)).action_apply()
+        return self.env.ref("crm.crm_lead_all_leads").read()[0]
     
     ####################
     # Business methods #

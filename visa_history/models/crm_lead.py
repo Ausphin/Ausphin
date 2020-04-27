d870ca993527dd3c75d41ef34b1e64c55b2e5c7c
Jimuel Atienza <jimuel.atienza@odevsolutions.com>
2020-04-27 15:10:31 +0000

    [FIX] crm_training: Fixed bugs after resolving merge conflict

--- visa_history/models/crm_lead.py
+++ visa_history/models/crm_lead.py
@@ -34,7 +34,6 @@ class CrmLead(models.Model):
             ("402", "402"),
             ("407", "407"),
             ("485", "485")],
-            ("leadership", "Leadership")],
         compute="_compute_visa_details",
         store=True)
     
@@ -54,15 +53,6 @@ class CrmLead(models.Model):
                 lead.visa_start_date = newest_visa.start_date
                 lead.visa_expiration_date = newest_visa.expiration_date
 
-    @api.depends("visa_ids", "visa_ids.expiration_date", "visa_ids.type")
-    def _compute_visa_details(self):
-        for lead in self:
-            lead.visa_type = False
-            lead.visa_expiration_date = False
-            visa_ids = lead.visa_ids.filtered(lambda l: l.start_date)
-            if visa_ids:
-                lead.visa_type = visa_ids[-1].type
-                lead.visa_expiration_date = visa_ids[-1].expiration_date
             
 
     ############################
